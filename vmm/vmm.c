4fb60208e9a6b8d7c1a96cc80d1ff139a8a0f61d
Amol Surati <asurati@users.noreply.github.com>
2019-05-21 18:47:35 +0530

    vmm: Prevent -Warray-bounds with -O3. Enable 2ndary page table search.

--- vmm/vmm.c
+++ vmm/vmm.c
@@ -11,22 +11,22 @@
 
 void vmm_part_init()
 {
-	extern char _parttab_org;
-	extern char _parttab_end;
+	extern char _parttab_org[];	/* [] to prevent -Warray-bounds */
+	extern char _parttab_end[];
 	extern char _htab_org;
 	struct part_entry *pe;
 	uint64_t v, w, ptab;
 	size_t sz;
 
 	/* Zero the partition table. */
-	sz = &_parttab_end - &_parttab_org;
-	memset(&_parttab_org, 0, sz);
+	sz = _parttab_end - _parttab_org;
+	memset(_parttab_org, 0, sz);
 
 	/*
 	 * Fill the first entry in the partition table,
 	 * for LPID = 0 = hypervisor.
 	 */
-	pe = (struct part_entry *)&_parttab_org;
+	pe = (struct part_entry *)_parttab_org;
 
 	w = EA_TO_RA(&_htab_org);
 	w >>= 18;	/* htaborg is high order 42 bits. */
@@ -44,7 +44,6 @@ void vmm_part_init()
 
 	mfspr(v, SPR_LPCR);
 	v |= bits_on(LPCR_AIL);	/* Enable AIL. */
-	v |= bits_on(LPCR_TC);	/* Disable 2ndary page table search. */
 
 	hwsync();	/* Order the updates. */
 
